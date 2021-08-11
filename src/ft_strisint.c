26a33689c9f2b65594df1c65871ccef0e933bb9f
happy_sadan <tristancottam.1@gmail.com>
2021-08-11 23:03:56 +0200

    fix: Fix ft_strisint() memory leaks

--- src/ft_strisint.c
+++ src/ft_strisint.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 14:03:26 by trcottam          #+#    #+#             */
-/*   Updated: 2021/08/09 15:27:05 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/08/11 23:03:12 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 bool	ft_strisint(char *str)
 {
 	bool	neg;
+	char	*tmp_itoa;
 
 	if (*str == '-')
 		neg = true;
@@ -22,9 +23,14 @@ bool	ft_strisint(char *str)
 		str++;
 	while (*str == '0')
 		str++;
-	if (ft_strcmp(str, ft_itoa(ft_atoi(str)))
+	tmp_itoa = ft_itoa(ft_atoi(str));
+	if (ft_strcmp(str, tmp_itoa)
 			&& !(!ft_strcmp(str, "2147483648") && neg))
+	{
+		free(tmp_itoa);
 		return (false);
+	}
+	free(tmp_itoa);
 	while (ft_isdigit(*str))
 		str++;
 	return (!*str);
