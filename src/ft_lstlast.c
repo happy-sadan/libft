6d7d1fa000345e15c85217e75a3ab5f8d153a13b
happy_sadan <tristancottam.1@gmail.com>
2021-09-15 17:40:36 +0200

    fix: Fix ft_lstlast()

--- src/ft_lstlast.c
+++ src/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 10:45:38 by trcottam          #+#    #+#             */
-/*   Updated: 2021/06/21 17:40:29 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/09/15 17:39:14 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
 		return (NULL);
-	if (!lst->next)
-		return (lst);
-	return (ft_lstlast(lst->next));
+	if (lst->prev)
+		return (lst->prev);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
